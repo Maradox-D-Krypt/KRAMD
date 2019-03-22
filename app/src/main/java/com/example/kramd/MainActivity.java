5ce11cdb83cb7857fdc015560adbec26d9a59ca6
Abba <abbagayrichards@gmail.com>
2019-03-22 12:02:57 -0500

    Layout

--- app/src/main/java/com/example/kramd/MainActivity.java
+++ app/src/main/java/com/example/kramd/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.kramd;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void displayRegistrationACtivity(View view) {
+        Intent myIntent = new Intent(this, RegistrationActivity.class);
+        startActivity(myIntent);
+    }
+
+    public void displayLogin(View view) {
+        Intent myIntent = new Intent(this, LoginActivity.class);
+        startActivity(myIntent);
     }
 }
